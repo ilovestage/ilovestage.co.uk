c838a49d092db5e20a83a5e2f3fefa1e800f0882
Karl Podger <karl.podger@primeordinal.com>
2014-09-12 21:56:39 +0100

    Set version 1 of the API as the default redirect.

--- applications/api.js
+++ applications/api.js
@@ -24,8 +24,8 @@ app.use(function *(next) {
 app.use(router(app));
 
 app.use(mount('/v1', api.v1.middleware()));
-app.use(mount('/v2', api.v2.middleware()));
+// app.use(mount('/v2', api.v2.middleware()));
 
-app.redirect('/', '/api/v2'); // show info page instead
+app.redirect('/', '/api/v1'); // show info page instead
 
 module.exports = app;
