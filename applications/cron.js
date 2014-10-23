2036a24d35d36a205574a36f418408939079952c
Karl Podger <karl.podger@primeordinal.com>
2014-10-23 01:12:56 +0100

    Enable cron job to find and charge bookings for imminent events.

--- applications/cron.js
+++ applications/cron.js
@@ -6,30 +6,41 @@ var config = packageJson.config.environment[process.env.NODE_ENV || 'development
 var _ = require('lodash');
 var argv = require('yargs').argv;
 var co = require('co');
-var database = require(__dirname + '/database');
 var koa = require('koa');
+var moment = require('moment');
+var stripe = require('stripe')(config.api.stripe.key);
+var thunkify = require('thunkify');
 var ua = require('universal-analytics');
+
+var database = require(__dirname + '/database');
+var utilities = require(__dirname + '/modules/utilities');
 
 var db = new database(config.server.database);
 
 var bookings = db.collection('bookings');
 var events = db.collection('events');
-var moment = require('moment');
 var payments = db.collection('payments');
 var shows = db.collection('shows');
 var users = db.collection('users');
+
+var createChargeThunk = thunkify(stripe.charges.create);
+var createChargeBoundThunk = createChargeThunk.bind(stripe.charges);
 
 var app = koa();
 
 var visitor = ua('UA-55818646-3');
 
-var result = null;
+// var result = null;
 var searchParameters = {};
+var returnFields = {};
+var updateFields = {};
+var insertFields = {};
 
 var ticketBookingThreshold = 10;
 // var ticketBookingThreshold = 2; //test
 
 var currentDate = new Date();
+var rangeStart = moment(currentDate).add(7, 'days').toDate();
 
 app.use(function* (next) {
   var job = argv.job.split('-');
@@ -42,67 +53,152 @@ app.use(function* (next) {
 
 switch(argv.job) {
   case 'bookings-finalise':
-    searchParameters.status = 'pending';
+    searchParameters.bookings = {
+      status: 'pending',
+      tickets: {
+        $gte: ticketBookingThreshold
+      }
+    };
 
-    searchParameters.tickets = {
-      $gte: ticketBookingThreshold
+    searchParameters.events = {
+      starttime: {
+        $gte: currentDate,
+        $lt: rangeStart
+      }
+    };
+
+    searchParameters.payments = {};
+
+    searchParameters.users = {};
+
+    returnFields.bookings = {
+      '_id': 1,
+      'eventid': 1,
+      'userid': 1
+    };
+
+    returnFields.events = {
+      '_id': 1,
+      'starttime': 1,
+      'discount_price': 1
+    };
+
+    returnFields.payments = {};
+
+    returnFields.users = {};
+
+    updateFields.bookings = {
+      $set: {}
+    };
+
+    updateFields.events = {};
+
+    updateFields.payments = {};
+
+    updateFields.users = {};
+
+    insertFields.payments = {
+      processor: 'stripe'
     };
 
     co(function *() {
-      var fields = {
-        $set: {
-          status: 'closed'
-        }
-      };
+      var booking = yield bookings.find(searchParameters.bookings, {
+        fields: returnFields.bookings
+      });
 
-      // result = yield bookings.find(searchParameters);
-      result = yield bookings.findAndModify(searchParameters, fields);
+      console.log('bookings: ' + booking.length, booking);
 
-      console.log('result', result);
-      _(result).forEach(function(doc) {
+      _(booking).forEach(function(doc) {
         co(function *() {
-          var rangeStart = moment(currentDate).add(7, 'days').toDate();
+          // console.log('doc', doc);
+          searchParameters.events._id = doc.eventid.toString(); //reset variable;
 
-          searchParameters = {}; //reset variable;
-          searchParameters._id = doc.eventid.toString();
-
-          searchParameters.starttime = {};
-          searchParameters.starttime.$gte = currentDate;
-          // searchParameters.starttime.$lt = rangeStart;
-
-          console.log('searchParameters', searchParameters);
-
-          var event = yield events.find(searchParameters, {
-            fields: {
-              '_id': 1,
-              'starttime': 1
-            }
+          var event = yield events.findOne(searchParameters.events, {
+            fields: returnFields.events
           });
 
-          //payment
-          var fields = {
-            $set: {
-              status: 'paid'
+          if (event) {
+            searchParameters.payments.eventid = doc.eventid.toString(); //reset variable;
+            searchParameters.users._id = doc.userid.toString(); //reset variable;
+
+            // var payment = yield payments.findOne(searchParameters.payments, {
+            //   fields: returnFields.payments
+            // });
+
+            var user = yield users.findOne(searchParameters.users, {
+              fields: returnFields.users
+            });
+
+            console.log('booking', booking);
+            console.log('event', event);
+            console.log('user', user);
+
+            if (user) {
+              var chargeInfo = {
+                amount: (event.discount_price * 100),
+                currency: 'gbp',
+                customer: user.stripeid,
+                // card: user.stripetoken,
+                metadata: {
+                  bookingid: doc._id.toString()
+                },
+                capture: 'true'
+                // description: booking.description
+              };
+
+              console.log('chargeInfo', chargeInfo);
+
+              insertFields.payments.bookingid = doc._id.toString();
+              insertFields.payments.token = user.stripeid;
+              insertFields.payments.time = currentDate;
+
+              try {
+                var charge = yield createChargeBoundThunk(chargeInfo);
+
+                if (charge) {
+                  console.log('charge', charge);
+
+                  insertFields.payments.status = 'success';
+                  insertFields.payments.response = charge;
+
+                  updateFields.bookings.$set.status = 'success';
+
+                  var payment = yield payments.insert(insertFields.payments);
+                  console.log('payment', payment);
+
+                  var updatedBooking = yield bookings.findAndModify(searchParameters.bookings, updateFields.bookings);
+                  console.log('updatedBooking', updatedBooking);
+
+                  utilities.sendEmail('user-booking', {
+                    subject: 'Booking target reached', // Subject line
+                    email: user.strategies.local.email
+                  });
+                }
+              } catch(error) {
+                console.log('error', error); // ENOTFOUND
+
+                insertFields.payments.status = 'failure';
+                insertFields.payments.error = error;
+
+                updateFields.bookings.$set.status = 'failure';
+
+                var payment = yield payments.insert(insertFields.payments);
+                console.log('payment', payment);
+
+                var updatedBooking = yield bookings.findAndModify(searchParameters.bookings, updateFields.bookings);
+                console.log('updatedBooking', updatedBooking);
+
+                utilities.sendEmail('admin-booking', {
+                  subject: 'Booking payment failed', // Subject line
+                  email: utilities.emailSender.address,
+                  message: error.message
+                });
+              }
+
             }
-          };
-
-          var payment = yield payments.findAndModify(searchParameters, {
-            '_id': 1,
-            'starttime': 1
-          });
-
-          // email = sendEmail('admin-booking', {
-          //   subject: 'Booking target reached', // Subject line
-          //   email: emailSender.address
-          // });
-
-          console.log('event', event);
-
-          if (event && event.length > 0) {
-            doc.event = event;
           }
 
-          console.log('doc', doc);
+          // console.log('doc', doc);
           return false;
         })();
       });
