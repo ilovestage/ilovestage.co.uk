a7936a0626eb0beaf96e877ef005685e1071332d
Karl Podger <karl.podger@primeordinal.com>
2014-10-27 16:58:08 +0000

    Simplify API versioning inclusion.

--- applications/api.js
+++ applications/api.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var packageJson = require(__dirname + '/../package.json');
-var config = packageJson.config.environment[process.env.NODE_ENV || 'development'];
 var environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 
 var koa = require('koa');
@@ -11,25 +10,22 @@ var ua = require('universal-analytics');
 
 var visitor = ua(packageJson.config.applications.api.googleanalytics.key);
 
-var api = [];
-api.v1 = require(__dirname + '/api/v1');
-api.v2 = require(__dirname + '/api/v2');
-
 var app = koa();
 
 app.use(router(app));
 
-if(environment !== 'development') {
-  app.use(function* (next) {
+app.use(function* (next) {
+  if(environment !== 'development') {
     visitor.pageview(this.request.originalUrl, function (err) {
       console.log('err', err);
     });
-    yield next;
-  });
-}
+  }
 
-app.use(mount('/v1', api.v1));
-// app.use(mount('/v2', api.v2));
+  yield next;
+});
+
+app.use(mount('/v1', require(__dirname + '/api/v1')));
+// app.use(mount('/v2', require(__dirname + '/api/v2')));
 
 app.redirect('/', '/v1');
 
