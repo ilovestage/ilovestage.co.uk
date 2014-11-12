9a51ca05c46db4d9b6f574701c2b32f59582dea6
Karl Podger <karl.podger@primeordinal.com>
2014-11-12 15:23:04 +0000

    Handle Debian's in-built Ruby Gem update method in preinstall script.

--- preinstall.sh
+++ preinstall.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+DISTRIB_ID=`cat /etc/lsb-release | sed -n 's/^DISTRIB_ID=//p'`
+
+#echo "DISTRIB_ID: $DISTRIB_ID"
+
 git config core.ignorecase false
 
-gem update --system
+if [ $DISTRIB_ID = 'Ubuntu' ]; then
+	sudo update_rubygems
+else
+	gem update --system
+fi
+
 gem install bundler
 bundle install
 
