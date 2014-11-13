df3e6d707ce14c153f2f28c574e8cbabad839229
Karl Podger <karl.podger@primeordinal.com>
2014-11-13 13:01:53 +0000

    Update nginx vhost configuration files.

--- server-configuration/pm2/start.js
+++ server-configuration/pm2/start.js
@@ -1,4 +1,7 @@
 pm2 start -f --node-args="--harmony --debug" app.js --name www -i max -- --application www
 pm2 start -f --node-args="--harmony --debug" app.js --name api -i max -- --application api
 
-pm2 start -f /var/www/ilovestage.co.uk/server-configuration/pm2/processes.json
+pm2 startup ubuntu
+pm2 save
+
+#pm2 start -f /var/www/ilovestage.co.uk/server-configuration/pm2/processes.json
