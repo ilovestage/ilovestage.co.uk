c040bd0e183c861ba96d818c404747fb0a25950f
Karl Podger <karl.podger@primeordinal.com>
2014-11-13 20:31:29 +0000

    Restart server from deployment bash script.

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,13 @@
 
 cd "$(dirname "$0")"
 
+echo "Syncing assets to Amazon S3 bucket:"
+aws s3 sync ./build/www s3://cdn.ilovestage.co.uk --delete --exclude "*.DS_Store"
+
 echo "Running 'git pull' from /var/www/ilovestage.co.uk directory on Amazon EC2 instance:"
 ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "(cd /var/www/ilovestage.co.uk && git pull)"
 
-echo "Syncing assets to Amazon S3 bucket:"
-aws s3 sync ./build/www s3://cdn.ilovestage.co.uk --delete --exclude "*.DS_Store"
+echo "Restart application on Amazon EC2 instance via PM2 graceful reload:"
+ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "pm2 gracefulReload all"
 
 exit
