a091944a700cd0513904a94c96b8a3413ff4d71c
Karl Podger <karl.podger@primeordinal.com>
2014-11-13 12:32:56 +0000

    Add sample PM2 configuration.

--- app.js
+++ app.js
@@ -1,82 +1,20 @@
 'use strict';
 
-function appListen() {
-  if(environment !== 'development') {
-    console.log('Detected staging or production environment.');
-    clusterOnAvailablePort();
-  } else {
-    console.log('Detected development environment on port ' + portStart + '.');
-    app.listen(portStart);
-  }
+function appListen(port) {
+  console.log('Detected process is running on port ' + port + '.');
+  app.listen(port);
 }
-
-function clusterOnAvailablePort() {
-  portscanner.findAPortNotInUse(portStart, portEnd, '127.0.0.1', function(error, availabePort) {
-    var port = availabePort;
-    console.log('Available port at ' + port);
-    // setup clustering
-    if (cluster.isMaster) {
-      // Fork workers
-      for (var i = 0, n = os.cpus().length; i < n; i += 1) {
-        cluster.fork();
-      }
-
-      cluster.on('exit', function(worker, code, signal) {
-        // When any of the workers die the cluster module will emit the 'exit' event.
-        // This can be used to restart the worker by calling .fork() again.
-        console.log('Worker ' + worker.process.pid + ' died');
-        cluster.fork();
-      });
-
-      cluster.on('fork', function(worker, code, signal) {
-        console.log('Worker ' + worker.process.pid + ' forked');
-      });
-
-      cluster.on('online', function(worker, code, signal) {
-        console.log('Worker ' + worker.process.pid + ' responded after it was forked');
-      });
-
-      console.info('Main application now running on http://localhost:' + port);
-    } else {
-      if (!module.parent) {
-        // set koa to listen on specified port
-        app.listen(port);
-      }
-    }
-  });
-}
-
-function serveAssets() {
-  // serve static files
-  app.use(serve(__dirname + '/' + packageJson.config.path.build), {
-    defer: true
-  }); // true web root
-  app.use(serve(__dirname + '/' + packageJson.config.path.source), {
-    defer: true
-  }); // to save copying bower_components, SASS files, etc.
-}
-
-var packageJson = require(__dirname + '/package.json');
-var environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 
 var argv = require('yargs').argv;
-var cluster = require('cluster');
-// var database = require(__dirname + '/applications/database');
 var debug = require('debug');
-// var http = require('http');
 var koa = require('koa');
 var logger = require('koa-logger');
 var mount = require('koa-mount');
-// var open = require('open');
-var os = require('os');
-var portscanner = require('portscanner');
 var router = require('koa-router');
-// var session = require('koa-session');
-var serve = require('koa-static');
 
-// var portStart = process.env.PORT ? process.env.PORT : packageJson.config.environment[environment].server.koa.port;
-var portStart = packageJson.config.applications[argv.application].port.start;
-var portEnd = packageJson.config.applications[argv.application].port.end;
+var packageJson = require(__dirname + '/package.json');
+var environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+var port = process.env.PORT ? process.env.PORT : packageJson.config.applications[argv.application].app.port;
 
 var app = koa();
 
@@ -102,45 +40,30 @@ app.use(router(app));
 
 app.poweredBy = false;
 
-// setup session
-// app.keys = ['secrets'];
-// app.use(session());
-
-// mount applications
-
 console.log('Requiring ' + argv.application + ' application.');
 
-// console.log('app argv', argv);
+application = require(__dirname + '/applications/' + argv.application);
 
 switch(argv.application) {
   case 'admin':
-    application = require(__dirname + '/applications/admin');
     app.use(mount('/', application));
-
-    serveAssets();
-    appListen();
+    appListen(port);
   break;
   case 'api':
-    application = require(__dirname + '/applications/api');
     app.use(mount('/', application));
-
-    appListen();
+    appListen(port);
   break;
   case 'cron':
-    application = require(__dirname + '/applications/cron');
-    // app.use(mount('/', application));
+
+  break;
+  case 'importer':
+
   break;
   case 'socket':
-    application = require(__dirname + '/applications/socket');
-    // app.use(mount('/', application));
-
-    appListen();
+    appListen(port);
   break;
   case 'www':
-    application = require(__dirname + '/applications/www');
     app.use(mount('/', application));
-
-    serveAssets();
-    appListen();
+    appListen(port);
   break;
 }
--- server-configuration/pm2/start.js
+++ server-configuration/pm2/start.js
@@ -1,11 +1,11 @@
-pm2 start /var/www/ilovestage.co.uk/server-configuration/pm2/processes.json
+pm2 start -f /var/www/ilovestage.co.uk/server-configuration/pm2/processes.json
+pm2 start -f server-configuration/pm2/processes.json
 
 pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application api
-pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application api
-pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application api
-pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application api
+pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application www
 
-pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application www
-pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application www
-pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application www
-pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application www
+pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application www -i max
+pm2 start --node-args="--harmony --debug" /var/www/ilovestage.co.uk/app.js -- --application api -i max
+
+pm2 start -f --node-args="--harmony --debug" app.js --name www -- --application www -i max
+pm2 start -f --node-args="--harmony --debug" app.js --name api -- --application api -i max
