416526306f22214dac768327a773282ca8c17f72
Karl Podger <karl.podger@primeordinal.com>
2014-11-14 12:36:30 +0000

    Add sample PM@ start command for admin application.

--- server-configuration/pm2/command-start.sh
+++ server-configuration/pm2/command-start.sh
@@ -2,6 +2,7 @@
 
 pm2 start -f --node-args="--harmony --debug" app.js --name www -i max -- --application www
 pm2 start -f --node-args="--harmony --debug" app.js --name api -i max -- --application api
+pm2 start -f --node-args="--harmony --debug" app.js --name admin -i max -- --application admin
 
 #pm2 start -f /var/www/ilovestage.co.uk/server-configuration/pm2/processes.json
 
