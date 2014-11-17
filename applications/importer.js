c9fa84e5ab53a700e2728854a6aa781d792f6384
Karl Podger <karl.podger@primeordinal.com>
2014-11-17 16:29:22 +0000

    Work in progress of reimplementation of importer application.

--- applications/importer.js
+++ applications/importer.js
@@ -6,13 +6,18 @@ var config = packageJson.config.environment[process.env.NODE_ENV || 'development
 var _ = require('lodash');
 var argv = require('yargs').argv;
 var co = require('co');
+var csv = require('csv');
+var fs = require('fs');
+var Glob = require("glob").Glob;
 var koa = require('koa');
-var moment = require('moment');
-var stripe = require('stripe')(config.api.stripe.key);
-var thunkify = require('thunkify');
+// var moment = require('moment');
+var path = require('path');
+// var should = require('should');
+// var stripe = require('stripe')(config.api.stripe.key);
+// var thunkify = require('thunkify');
 var ua = require('universal-analytics');
 
-var database = require(__dirname + '/database');
+var database = require(__dirname + '/_utilities/database');
 var utilities = require(__dirname + '/_utilities/utilities');
 
 var db = new database(config.server.database);
@@ -22,6 +27,16 @@ var app = koa();
 var visitor = ua(packageJson.config.applications.importer.googleanalytics.key);
 
 var currentDate = new Date();
+
+var promise = {
+  db: {
+    shows: co.wrap(function* (val) {
+      return yield Promise.resolve(val);
+    })
+  }
+};
+
+
 
 app.use(function* (next) {
   var job = argv.job.split('-');
@@ -34,6 +49,57 @@ app.use(function* (next) {
 
 switch(argv.job) {
   case 'shows-all':
+    var pattern = path.resolve(__dirname, '../data/import/shows/*.csv');
+    console.log('pattern', pattern);
+
+    var files = new Glob(
+      pattern,
+      {
+        sync: true
+      }
+    );
+
+    // console.log('files', files);
+
+    // _(files).forEach(function(doc) {
+    //   console.log('doc', doc);
+    // });
+
+    // var empty = yield db.collection('shows').remove({});
+    db.collection('shows').remove({});
+
+    // var output = [];
+    var transformer = csv.transform(function(data) {
+      data.push(data.shift());
+      return data;
+    });
+
+    transformer.on('readable', function(){
+      console.log('readable');
+      var row;
+      while(row = transformer.read()) {
+        console.log('row', row);
+        // output.push(row);
+
+        promise.db.shows(true).then(function (row) {
+          console.log(row);
+        });
+      }
+    });
+
+    transformer.on('error', function(err) {
+      console.log(err.message);
+    });
+
+    transformer.on('finish', function(){
+      // output.should.eql([ [ '2', '3', '4', '1' ], [ 'b', 'c', 'd', 'a' ] ]);
+    });
+
+    transformer.write(['1','2','3','4']);
+    transformer.write(['a','b','c','d']);
+
+    transformer.end();
+
 
   break;
 }
