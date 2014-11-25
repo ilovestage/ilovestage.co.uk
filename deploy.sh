3f66f32431aa7459059b67d4baae210bd107b6e4
Karl Podger <karl.podger@primeordinal.com>
2014-11-25 23:55:23 +0000

    Run npm install and npm update in deploy shell script.

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,9 @@ aws s3 sync ./build/www s3://cdn.ilovestage.co.uk --delete --exclude "*.DS_Store
 echo "Running 'git pull' from /var/www/ilovestage.co.uk directory on Amazon EC2 instance:"
 ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "(cd /var/www/ilovestage.co.uk && git pull)"
 
+echo "Running 'git pull' from /var/www/ilovestage.co.uk directory on Amazon EC2 instance:"
+ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "(cd /var/www/ilovestage.co.uk && npm install && npm update)"
+
 echo "Restart application on Amazon EC2 instance via PM2 graceful reload:"
 ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "pm2 gracefulReload all"
 
