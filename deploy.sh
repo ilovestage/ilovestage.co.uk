c6460663fac94b6d413bdf792dee1b5dbc530f6f
Karl Podger <karl.podger@primeordinal.com>
2014-11-27 09:50:55 +0000

    Stop and restart server via deploy script to reload updated files into memory.

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,10 @@ ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "(cd /var/www/ilovestage.co.uk &&
 echo "Running 'git pull' from /var/www/ilovestage.co.uk directory on Amazon EC2 instance:"
 ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "(cd /var/www/ilovestage.co.uk && npm install && npm update)"
 
-echo "Restart application on Amazon EC2 instance via PM2 graceful reload:"
-ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "pm2 gracefulReload all"
+#echo "Restart application on Amazon EC2 instance via PM2 graceful reload:"
+#ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "pm2 gracefulReload all"
+
+echo "Restart application on Amazon EC2 instance via PM2 stop follow by PM2 restart:"
+ssh -i ~/.ssh/karl.pem ubuntu@ilovestage.co.uk "pm2 stop all && pm2 restart all"
 
 exit
