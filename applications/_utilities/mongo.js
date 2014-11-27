9bb2da27d656ce004fae7b39b2b9d74d1590a897
Karl Podger <karl.podger@primeordinal.com>
2014-11-27 09:38:53 +0000

    Tidy comments from mongo utility.

--- applications/_utilities/mongo.js
+++ applications/_utilities/mongo.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// var packageJson = require(__dirname + '/../../package.json');
-// var environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
-
 var bson = require('bson');
 
 var mongo = {
@@ -48,11 +45,6 @@ var mongo = {
 
   validateObjectId: function(id) {
     var bool = false;
-
-    // if(id.length === 24) {
-    //   bool = /[a-f]+/.test(id);
-    // }
-    // console.log('obj:', bson.BSONPure.ObjectID);
 
     try {
       bool = bson.BSONPure.ObjectID(id);
