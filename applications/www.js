e57758abea996b2b575651147438138e405ef890
Karl Podger <karl.podger@primeordinal.com>
2014-12-02 00:40:30 +0000

    Update routes and templates for password reset process.

--- applications/www.js
+++ applications/www.js
@@ -8,16 +8,19 @@ require('./_utilities/auth');
 var _ = require('lodash');
 // var _.str = require('underscore.string');
 var argv = require('yargs').argv;
-var email = require('_utilities/email');
+// var email = require('_utilities/email');
 var koa = require('koa');
 // var mount = require('koa-mount');
 // var bodyParser = require('koa-bodyparser');
-var passport = require('koa-passport');
+// var passport = require('koa-passport');
+var qs = require('koa-qs');
 var router = require('koa-router');
 var serve = require('koa-static');
-var session = require('koa-generic-session');
-var redisStore = require('koa-redis');
+// var session = require('koa-generic-session');
+// var redisStore = require('koa-redis');
 var views = require('co-views');
+
+var currentDate = new Date();
 
 var render = views('source/' + argv.application + '/views', {
   cache: true,
@@ -33,6 +36,7 @@ var defaults = {
     assets: packageJson.config.environment[environment].url.assets
   },
   lang: 'en',
+  year: currentDate.getFullYear(),
   title: 'I Love Stage',
   description: 'I Love Stage'
 };
@@ -41,6 +45,8 @@ var app = koa();
 
 app.name = argv.application;
 app.keys = ['keys', packageJson.config.redis.key];
+
+qs(app);
 
 // app.use(session({
 //   store: redisStore({
@@ -80,16 +86,22 @@ if (environment === 'development') {
 // }
 
 function *error404(next) {
-  var settings;
+  var settings = {};
 
   var locals = {
     bodyClass: 'error error-404',
     title: 'Page not found'
   };
 
-  _.merge(settings, [defaults, locals]);
+  _.merge(settings, defaults, locals);
 
-  this.body = yield render('error-404', settings);
+  var body = yield render('error-404', settings);
+
+  settings.body = body;
+
+  var html = yield render('layouts/default', settings);
+
+  this.body = html;
   this.status = 404;
 }
 
@@ -135,13 +147,71 @@ function *passwordReset(next) {
   var settings = {};
 
   var locals = {
-    bodyClass: 'privacy',
-    title: 'Password Reset'
+    bodyClass: 'privacy password-reset',
+    title: 'Reset Password'
   };
 
   _.merge(settings, defaults, locals);
 
   var body = yield render('password-reset', settings);
+
+  settings.body = body;
+
+  var html = yield render('layouts/default', settings);
+
+  this.body = html;
+}
+
+function *passwordResetEmailSent(next) {
+  var settings = {};
+
+  var locals = {
+    bodyClass: 'privacy password-reset password-reset-email-sent',
+    title: 'Password Reset Email Sent'
+  };
+
+  _.merge(settings, defaults, locals);
+
+  var body = yield render('password-reset-email-sent', settings);
+
+  settings.body = body;
+
+  var html = yield render('layouts/default', settings);
+
+  this.body = html;
+}
+
+function *passwordResetSuccess(next) {
+  var settings = {};
+
+  var locals = {
+    bodyClass: 'privacy password-reset password-reset-success',
+    title: 'Password Has Been Successfully Reset'
+  };
+
+  _.merge(settings, defaults, locals);
+
+  var body = yield render('password-reset-success', settings);
+
+  settings.body = body;
+
+  var html = yield render('layouts/default', settings);
+
+  this.body = html;
+}
+
+function *passwordResetVerification(next) {
+  var settings = {};
+
+  var locals = {
+    bodyClass: 'privacy password-reset password-reset-verification',
+    title: 'Verify Password Reset Request',
+    token: this.query.token
+  };
+
+  _.merge(settings, defaults, locals);
+
+  var body = yield render('password-reset-verification', settings);
 
   settings.body = body;
 
@@ -210,13 +280,15 @@ function *termsOfService(next) {
 }
 
 app.get('/', home);
-app.get('/account', account);
+app.get('/account', error404);
 app.get('/privacy-policy', privacyPolicy);
 app.get('/refund-policy', refundPolicy);
 app.get('/terms-of-service', termsOfService);
-app.get('/account/password-reset/', error404);
-app.get('/account/password-reset/:token', passwordReset);
+app.get('/account/password-reset', passwordReset);
+app.post('/account/password-reset/email-sent', passwordResetEmailSent);
+app.get('/account/password-reset/verification', passwordResetVerification);
+app.post('/account/password-reset/success', passwordResetSuccess);
 
-app.get(/^([^.]+)$/, error404); //matches everything without an extension
+app.all(/^([^.]+)$/, error404); //matches everything without an extension
 
 module.exports = app;
