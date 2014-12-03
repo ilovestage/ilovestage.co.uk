af17d7349d799de2f6315b1d80893584adf10b84
Karl Podger <karl.podger@primeordinal.com>
2014-12-03 21:59:32 +0000

    Add server configuration files.

---
+++ server-configuration/cron/mongodump.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MONGO_DATABASE="ilovestage"
+APP_NAME="ilovestage"
+
+MONGO_USER="ilovestage"
+MONGO_PASSWORD="curtaincall1"
+
+MONGO_HOST="ds063889.mongolab.com"
+MONGO_PORT="63889"
+TIMESTAMP=`date +%F-%H%M`
+MONGODUMP_PATH="/usr/bin/mongodump"
+BACKUPS_DIR="$HOME/Sites/ilovestage.co.uk/backups/$APP_NAME"
+BACKUP_NAME="$APP_NAME-$TIMESTAMP"
+
+# mongo admin --eval "printjson(db.fsyncLock())"
+$MONGODUMP_PATH -h $MONGO_HOST:$MONGO_PORT -d $MONGO_DATABASE -u $MONGO_USER -p $MONGO_PASSWORD
+# $MONGODUMP_PATH -h $MONGO_HOST:$MONGO_PORT -d $MONGO_DATABASE
+# $MONGODUMP_PATH -d $MONGO_DATABASE
+# mongo admin --eval "printjson(db.fsyncUnlock())"
+
+mkdir -p $BACKUPS_DIR
+mv dump $BACKUP_NAME
+tar -zcvf $BACKUPS_DIR/$BACKUP_NAME.tgz $BACKUP_NAME
+rm -rf $BACKUP_NAME
