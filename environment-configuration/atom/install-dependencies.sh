d96b11ac8ae4a296c568fe857c50672ba74bb70c
Karl Podger <karl.podger@primeordinal.com>
2014-12-05 12:53:21 +0000

    Refactor layout of environment settings folder.

---
+++ environment-configuration/atom/install-dependencies.sh
@@ -0,0 +1,3 @@
+npm install -g jshint
+npm install -g jsonlint
+npm install -g tslint
---
+++ environment-configuration/environment/posix.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+npm cache clean -f
+
+sudo npm update -g npm
+
+npm install -g gulp
+npm install -g imagemin
+npm install -g jshint
+npm install -g jsonlint
+npm install -g n
+npm install -g node-inspector
+npm install -g npm
+npm install -g npm-update-all
+npm install -g npmedge
+npm install -g pm2
+npm install -g strongloop
+
+n latest
+
+node -v
+
+exit
